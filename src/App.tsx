be6febd8b733d2c85a8b930afb82272a0510945d
joeyanovich <joedison.dias@gmail.com>
2023-07-10 10:42:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Menu } from "./components/Menu";
+
+import './style/global.css'
+
+export function App() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  )
+}
---
+++ src/components/Menu.tsx
@@ -0,0 +1,7 @@
+export function Menu() {
+    return (
+        <div>
+            He
+        </div>
+    )
+}
