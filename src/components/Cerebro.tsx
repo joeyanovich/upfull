7a575cf8468a76a7224e38e3374f6eada7fdbae6
unknown <lucastoloto.silva@gmail.com>
2023-07-10 11:59:06 -0300

    Update

--- src/components/Cerebro.tsx
+++ src/components/Cerebro.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 export function Cerebro (){
     const [currentFrame, setCurrentFrame] = useState(1);
     const [isReversed, setIsReversed] = useState(false);
-  
+
     useEffect(() => {
       const interval = setInterval(() => {
         if (!isReversed) {
@@ -17,14 +18,22 @@ export function Cerebro (){
             setIsReversed(false);
           }
         }
-      }, 30);
+      }, 60 + Math.floor(Math.random() * 21));
   
       return () => clearInterval(interval);
     }, [currentFrame, isReversed]);
-  
+
+    
   return (
     <div className="w-full h-full">
-      <img src={`/src/assets/img/brain/${currentFrame}.png`} alt="Brain Frame" />
+      <motion.img
+        src={`/src/assets/img/brain/${currentFrame}.png`}
+        className='transition ease-in-out delay-150'
+        alt="Brain Frame"
+        initial={{ opacity: 0, scale: 0.9 }} 
+        animate={{ opacity: 1, scale: 1 }} 
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+      />
     </div>
   );
 };
